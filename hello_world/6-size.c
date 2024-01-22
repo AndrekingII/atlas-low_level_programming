e304e2f82c2ac36d2a743f13a78e8793240f6455
AndrekingII <andre.king@okstate.edu>
2024-01-22 20:40:35 +0000

    list

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -8,6 +13,4 @@ int main(void)
     printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
     printf("Size of a float: %zu byte(s)\n", sizeof(float));
     return (0);
-
-}
-
+}    
